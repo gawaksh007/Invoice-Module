a1534aea00f358546e491ffbf4bd9b934fa53ee1
gawaksh yadav <gawakshyadav@gmail.com>
2018-03-20 18:46:07 +0530

    added new files

---
+++ src/Controller/InvoiceController.php
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * Contains \Drupal\invoice\Controller\InvoiceController.
+ */
+
+namespace Drupal\invoice\Controller;
+
+/**
+ * Controller routines for invoice routes.
+ */
+class InvoiceController {
+
+  public function Invoices() {
+    // ...
+  }
+  
+  public function InvoiceEdit() {
+    // ...
+  }
+
+  public function InvoicePrint() {
+    // ...
+  }
+
+  public function Invoicepdf() {
+    // ...
+  }
+
+  
+}
